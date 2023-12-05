7cb9f0e36629001e590329790543226e5c9504ae
DrewStats <135686675+DrewStats@users.noreply.github.com>
2023-12-05 17:58:27 -0400

    tidy

--- Pages/Controllers/Tree.mjs
+++ Pages/Controllers/Tree.mjs
@@ -337,3 +337,69 @@ loadButton.addEventListener('change', function (event) {
     }
 });
 document.body.appendChild(loadButton);
+
+
+// script.js
+
+class Nodo {
+    constructor(valor) {
+        this.valor = valor;
+        this.izquierda = null;
+        this.derecha = null;
+    }
+}
+
+function inOrder(node, result) {
+    if (node) {
+        inOrder(node.izquierda, result);
+        result.push(node.valor);
+        inOrder(node.derecha, result);
+    }
+}
+
+function preOrder(node, result) {
+    if (node) {
+        result.push(node.valor);
+        preOrder(node.izquierda, result);
+        preOrder(node.derecha, result);
+    }
+}
+
+
+function generarPostOrder() {
+    const preOrderInput = document.getElementById('preOrderInput').value.trim();
+    const inOrderInput = document.getElementById('inOrderInput').value.trim();
+
+    const preOrderList = preOrderInput.split(',').map(item => parseInt(item.trim()));
+    const inOrderList = inOrderInput.split(',').map(item => parseInt(item.trim()));
+
+    const raiz = construirArbol(preOrderList, inOrderList);
+
+    const postOrderList = [];
+    postOrderFromInPre(inOrderList, preOrderList).forEach(valor => {
+        postOrderList.push(valor);
+    });
+
+    document.getElementById('resultado').textContent = 'Post-Order: ' + postOrderList.join(', ');
+}
+
+function construirArbol(preOrderList, inOrderList) {
+    if (preOrderList.length === 0 || inOrderList.length === 0) {
+        return null;
+    }
+
+    const rootValue = preOrderList[0];
+    const rootIndexInOrder = inOrderList.indexOf(rootValue);
+
+    const inOrderLeft = inOrderList.slice(0, rootIndexInOrder);
+    const inOrderRight = inOrderList.slice(rootIndexInOrder + 1);
+
+    const preOrderLeft = preOrderList.slice(1, rootIndexInOrder + 1);
+    const preOrderRight = preOrderList.slice(rootIndexInOrder + 1);
+
+    const node = new Nodo(rootValue);
+    node.izquierda = construirArbol(preOrderLeft, inOrderLeft);
+    node.derecha = construirArbol(preOrderRight, inOrderRight);
+
+    return node;
+}
